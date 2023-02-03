f89ae78e55bec2cce19f08938d4efe0a788ecc4b
LionelCoutinot <69722628+LionelCoutinot@users.noreply.github.com>
2023-02-03 19:11:06 +0100

    Update PurchaseType.php

--- src/Form/PurchaseType.php
+++ src/Form/PurchaseType.php
@@ -16,7 +16,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PurchaseType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void /* Définition des divers types de champs pour le fomulaire de commande et des paramètres qui leur sont assignés si besoin */
+    public function buildForm(FormBuilderInterface $builder, array $options): void /* Définition des divers types de champs pour le formulaire de commande et des paramètres qui leur sont assignés si besoin */
     {
         $builder
             ->add('firstname',  TextType::class)
